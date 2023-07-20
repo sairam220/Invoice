211956c3d418b1a8e53c1e76aa7aeacda9a52669
Sairam Gudida <gudidasairam123@gmail.com>
2023-07-20 10:44:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [invoiceData, setInvoiceData] = useState({
+    invoicedate: '',
+    invoicenumber: '',
+    invoiceamount: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInvoiceData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('http://localhost:5001/api/add_invoice', invoiceData);
+      alert('Invoice added successfully');
+      setInvoiceData({
+        invoicedate: '',
+        invoicenumber: '',
+        invoiceamount: '',
+      });
+    } catch (error) {
+      console.error('Error:', error.response.data.error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add Invoice</h1>
+      <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <label for='date'>Invoice Date</label>
+          <input
+            type="date"
+            name="invoicedate"
+            id='date'
+            value={invoiceData.invoicedate}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label for="number">Invoice Number</label>
+          <input
+            type="text"
+            id='number'
+            name="invoicenumber"
+            value={invoiceData.invoicenumber}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label for="amount">Invoice Amount</label>
+          <input
+            type="number"
+            name="invoiceamount"
+            id='amount'
+            value={invoiceData.invoiceamount}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className='button-83'>Add Invoice</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
